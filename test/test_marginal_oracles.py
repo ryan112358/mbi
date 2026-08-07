9c636df4c053ef2782651b1316ed41161d6dc39f
mckennar <mckennar@google.com>
2026-08-07 17:17:20 +0000

    Update test_marginal_oracles.py to pass frozendict evidence

--- test/test_marginal_oracles.py
+++ test/test_marginal_oracles.py
@@ -1,26 +1,23 @@
+import functools
+import itertools
 import unittest
-from mbi.domain import Domain
-from mbi.factor import Factor
-from mbi.clique_vector import CliqueVector
-from mbi import marginal_oracles
-from mbi.marginal_oracles import (
-    message_passing_implicit,
-    message_passing_hugin,
-    message_passing_shafer_shenoy,
-    einsum_materialized,
-    einsum_fused,
-    einsum_semistable,
-)
-from mbi.constraint import Constraint
-from mbi.extensions.message_passing import (
-    shafer_shenoy as ext_shafer_shenoy,
-    implicit as ext_implicit,
-)
+
+import frozendict
 import jax.numpy as jnp
 import numpy as np
 from parameterized import parameterized
-import itertools
-import functools
+
+from mbi import marginal_oracles
+from mbi.clique_vector import CliqueVector
+from mbi.constraint import Constraint
+from mbi.domain import Domain
+from mbi.extensions.message_passing import implicit as ext_implicit
+from mbi.extensions.message_passing import shafer_shenoy as ext_shafer_shenoy
+from mbi.factor import Factor
+from mbi.marginal_oracles import (einsum_fused, einsum_materialized,
+                                  einsum_semistable, message_passing_hugin,
+                                  message_passing_implicit,
+                                  message_passing_shafer_shenoy)
 
 np.random.seed(0)
 
@@ -168,7 +165,7 @@ class TestMarginalOracles(unittest.TestCase):
     theta = CliqueVector.random(_DOMAIN, model_cliques)
     evidence_attr = _DOMAIN.attributes[0]
     evidence_val = 0
-    evidence = {evidence_attr: evidence_val}
+    evidence = frozendict.frozendict({evidence_attr: evidence_val})
 
     if evidence_attr in query_clique:
       with self.assertRaises(ValueError):
